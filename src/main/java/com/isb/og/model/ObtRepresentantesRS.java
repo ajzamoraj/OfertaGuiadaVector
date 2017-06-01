9a0d090a1734401cbb11016698175900eb5aa039
jperezg <jperezg@vectoritcgroup.com>
2017-06-01 11:27:51 +0200

    Se crea el bean de salida del servicio rest ObtRepresentantes

---
+++ src/main/java/com/isb/og/model/ObtRepresentantesRS.java
@@ -0,0 +1,16 @@
+package com.isb.og.model;
+
+import java.io.Serializable;
+
+import com.isb.og.common.model.ModelError;
+
+public class ObtRepresentantesRS extends ModelError implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
