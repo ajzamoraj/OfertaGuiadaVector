a295d5a08ab2178e6f91827b4c17194a90fd610d
jperezg <jperezg@vectoritcgroup.com>
2017-06-01 11:24:09 +0200

    Se crea el bean de entrada del servicio rest ObtRepresentantes

---
+++ src/main/java/com/isb/og/model/ObtRepresentantesRQ.java
@@ -0,0 +1,14 @@
+package com.isb.og.model;
+
+import com.isb.og.security.model.SeguridadTokenBean;
+
+public class ObtRepresentantesRQ extends SeguridadTokenBean {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
